a66d94bc7a2639000f2f3a386ec5c1a4015a178f
tiger-eagle <elaineelainefletcher@gmail.com>
2023-09-18 12:00:04 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
